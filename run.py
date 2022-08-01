2cb6492c9cd2c6e9bbc208986db9b9b296c9f1b1
assofiejakobsson <s89j@live.se>
2022-08-01 11:44:09 +0000

    Add some coments to strukter the game and add the game board

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-", ]
+
+# Welcome
+
+# Make a choise
+
+# Game rules
+
+# Exit the game
+
+# Start the game
+
+
+def displayBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+print(displayBoard(board))
+
+# Pleas Enter your name
+
+# Check If there is tree in a row horizontally
+
+# Check If there is tree in a row vertically
+
+# Check If there is tree in a row diagonally
+
+# Check for wrong input and print error message
+
+# Message Win, lose or a tie
