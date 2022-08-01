6dd4e01d5c11b7efeab952035106e6ff9fbac3d2
assofiejakobsson <s89j@live.se>
2022-08-01 12:35:02 +0000

    Add player choise

--- run.py
+++ run.py
@@ -2,9 +2,24 @@ board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-", ]
 
+player = "x"
+computer = "o"
+
 # Welcome
 
 # Make a choise
+
+
+def playerChoise(board):
+    inp = int(input("Enter a number between 0-9: "))
+    if inp >= 0 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = player
+    else:
+        print("The spot is alredy taken!")
+
+
+print(playerChoise(board))
+
 
 # Game rules
 
@@ -17,6 +32,7 @@ def displayBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
+
 
 print(displayBoard(board))
 
